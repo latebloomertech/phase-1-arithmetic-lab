2fe2e50cd8454d2749f4e7fe41ab84cffc338712
Dan Baechtold <latebloomertech@gmail.com>
2022-01-10 00:31:57 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+let num3 = 19;
+let num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 4, 20);
+
+const random = math.random * 100;
